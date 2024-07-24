75770a5ad6086163720f61299ff00eb40918a753
Sofia Bargues <barguessofia@gmail.com>
2024-07-24 22:19:47 +0100

    colors with biscuit

--- src/App.jsx
+++ src/App.jsx
@@ -35,22 +35,22 @@ function App() {
   return (
     <div className="bg-[#1f2633] font-manrope h-screen  flex flex-col justify-center ">
       <div className="bg-[#303a4a] pb-[55px] h-[332px] w-[343px] md:h-[332px] md:w-[540px] m-auto rounded-xl justify-evenly p-[24px] flex flex-col items-center relative ">
-        <h3 className=" text-xs tracking-[0.3rem]  text-[#52ffa8]">
+        <h3 className=" text-xs tracking-[0.3rem]  text-neonGreen">
           ADVICE # {adviceNum}
         </h3>
-        <div className="text-[#cfe3e8] font-bold text-2xl text-center">
+        <div className="text-lightCyan font-bold text-2xl text-center">
           {advice}
         </div>
         {/* separator */}
         <div className="  flex flex-row gap-2 justify-center items-center ">
-          <div className="bg-[#cfe3e8]  h-[1px] w-28 md:w-[200px]"></div>
-          <div className="h-5 rounded-md w-[6px] bg-[#cfe3e8]"></div>
-          <div className="h-5 rounded-md w-[6px] bg-[#cfe3e8]"></div>
-          <div className="bg-[#cfe3e8]  h-[1px] w-28 md:w-[200px]"></div>
+          <div className="bg-lightCyan  h-[1px] w-28 md:w-[200px]"></div>
+          <div className="h-5 rounded-md w-[6px] bg-lightCyan"></div>
+          <div className="h-5 rounded-md w-[6px] bg-lightCyan"></div>
+          <div className="bg-lightCyan  h-[1px] w-28 md:w-[200px]"></div>
         </div>
         <button
           onClick={handleClick}
-          className="bg-[#52ffa8] hover:shadow-[#52ffa8_0px_0px_30px_0px]  absolute left-[130px] top-[280px]  md:left-[230px] md:top-[280px] rounded-full w-[80px] aspect-square flex flex-col justify-center items-center"
+          className="bg-neonGreen hover:shadow-[#52ffa8_0px_0px_30px_0px]  absolute left-[130px] top-[280px]  md:left-[230px] md:top-[280px] rounded-full w-[80px] aspect-square flex flex-col justify-center items-center"
         >
           <img className="h-8" src={dice} alt="" />
         </button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      lightCyan: "#cfe3e8",
+      neonGreen: "#52ffa8",
+      // ### Primary
+      // - Light Cyan: hsl(193, 38%, 86%)#cfe3e8
+      // - Neon Green: hsl(150, 100%, 66%)#52ffa8
+      // ### Neutral
+      // - Grayish Blue: hsl(217, 19%, 38%) #4e5d73
+      // - Dark Grayish Blue: hsl(217, 19%, 24%) #303a4a
+      // - Dark Blue: hsl(218, 23%, 16%) #1f2633
+    },
     extend: {
       fontFamily: { manrope: ["Manrope", "sans-serif"] },
     },
